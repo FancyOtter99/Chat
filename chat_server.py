37d23d6ddb29a694581bd9b488bd3f06511c2696
FancyOtter99 <pizza.great@protonmail.com>
2025-05-03 13:18:48 -0400

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -340,7 +340,7 @@ async def websocket_handler(request):
                     print("game started")
                     for client_ws in connected_clients.values():
                         if not client_ws.closed:
-                            await client_ws.send_json({"type": "game_started", "message": "Game has started", "game": data["selectedGame"]
+                            await client_ws.send_json({"type": "game_started", "message": "Game has started", "game": data["game"]
 })
 
                     
