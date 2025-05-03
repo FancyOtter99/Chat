a74ee47ae31d42a1c4acf21f5f4f116a4639e439
FancyOtter99 <pizza.great@protonmail.com>
2025-05-03 18:24:59 -0400

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -360,10 +360,10 @@ async def websocket_handler(request):
                             "game": data["game"],
                             "time": data["time"]
                         })
-                     elif data["game"] == "guess_the_pin":
+                    elif data["game"] == "guess_the_pin":
                         for user, client_ws in connected_clients.items():
-                            if not client_ws.closed:
-                                await client_ws.send_json({"type": "1game_finished", "finisher": data["sender"], "game": data["game"], "correctPin": data["realPin"] })
+                        if not client_ws.closed:
+                        await client_ws.send_json({"type": "1game_finished", "finisher": data["sender"], "game": data["game"], "correctPin": data["realPin"] })
 
                     
 
